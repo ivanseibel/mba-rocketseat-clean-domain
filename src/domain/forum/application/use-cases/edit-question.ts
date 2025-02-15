eec1ce34bb64aea5650874b49702ae46521c156f
Ivan Seibel <me@ivanseibel.dev>
2025-02-15 13:10:54 +0000

    implement EditQuestion use case; add save method to QuestionsRepository and corresponding tests

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,39 @@
+import type { QuestionsRepository } from "../repositories/questions-repository";
+
+interface EditQuestionUseCaseRequest {
+	authorId: string;
+	questionId: string;
+	title: string;
+	content: string;
+}
+
+// biome-ignore lint/complexity/noBannedTypes: <explanation>
+type EditQuestionUseCaseResponse = {};
+
+export class EditQuestionUseCase {
+	constructor(private questionsRepository: QuestionsRepository) {}
+
+	async execute({
+		authorId,
+		questionId,
+		title,
+		content,
+	}: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+		const question = await this.questionsRepository.findById(questionId);
+
+		if (!question) {
+			throw new Error("Question not found.");
+		}
+
+		if (authorId !== question.authorId.toString()) {
+			throw new Error("Not allowed.");
+		}
+
+		question.title = title;
+		question.content = content;
+
+		await this.questionsRepository.save(question);
+
+		return {};
+	}
+}
